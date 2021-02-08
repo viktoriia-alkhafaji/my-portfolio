67f31ea81197ef88d89a9d1b8ebbcb929721445a
viktoriia-alkhafaji <78023870+viktoriia-alkhafaji@users.noreply.github.com>
2021-02-08 20:20:22 +0100

    logic for entering e-mail and massage

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 let massageForm = document.querySelector('#massage-form');
 let emailForm = document.querySelector('#email-form');
 
-massageForm.keyup = abilityOfSendButton;
-emailForm.onchange = abilityOfSendButton;
+massageForm.onkeyup = abilityOfSendButton;
+emailForm.onkeyup = abilityOfSendButton;
 
 function abilityOfSendButton() {
     if (massageForm.value !== '' && emailForm.value !== '') {
@@ -10,7 +10,6 @@ function abilityOfSendButton() {
     }
     else if (massageForm.value == '' || emailForm.value == '') {
         submitButton.disabled = true;
-        console.log('Fill in all the fields, please!');
     }
 }
 
@@ -19,4 +18,18 @@ let submitButton = document.querySelector('#submit-button');
 submitButton.disabled = true;
 submitButton.onclick = function (event) {
     event.preventDefault();
+    if(emailForm.value.includes('@')){
+        console.log('Thank you for your massage');
+    }
+    else {
+        console.log('Please fill in the correct e-mail adress');
+    }
+    
 }
+
+
+/* submitButton.addEventListener('click', function(e) {
+    e.preventDefault();
+    console.log('work');
+})
+ */
